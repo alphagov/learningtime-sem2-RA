51dc21b3d4609c8b1846f8e3d28567e3e8d57dae
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-07-13 22:41:07 +0100

    added dummy routes to backend. This is done to test backend and frontend integration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express'
+import express, { Express, Request, Response } from 'express'
 import path from 'path'
 import * as bodyParser from 'body-parser'
 
@@ -8,6 +8,14 @@ const port = 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request: Request, response: Response) => {
+    response.json({ hello: 'world' })
+})
+
+app.get('/goodbye', (request: Request, response: Response) => {
+    response.json({ goodbye: 'world' })
+})
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
