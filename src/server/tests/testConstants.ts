0bfd903c2bb6d263e44dabf184ec0e9c8c4a0eb5
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-07-20 12:12:54 +0100

    modified test constants

--- src/server/tests/testConstants.ts
+++ src/server/tests/testConstants.ts
@@ -29,110 +29,76 @@ export const TEST_200_FETCH_RESPONSE: Response = {
         return ''
     }
 }
-export const TEST_POSTCODE_API_RESPONSE: Response = {
+export const TEST_POSTCODE_API_RESPONSE = {
     status: 200,
-    json: async () => {
-        return {
-            status: 200,
-            result: {
-                postcode: 'SW1A 2AA',
-                quality: 1,
-                eastings: 530047,
-                northings: 179951,
-                country: 'England',
-                nhs_ha: 'London',
-                longitude: -0.127695,
-                latitude: 51.50354,
-                european_electoral_region: 'London',
-                primary_care_trust: 'Westminster',
-                region: 'London',
-                lsoa: 'Westminster 018C',
-                msoa: 'Westminster 018',
-                incode: '2AA',
-                outcode: 'SW1A',
-                parliamentary_constituency: 'Cities of London and Westminster',
-                admin_district: 'Westminster',
-                parish: 'Westminster, unparished area',
-                admin_county: null,
-                date_of_introduction: '198001',
-                admin_ward: 'St. James\'s',
-                ced: null,
-                ccg: 'NHS North West London',
-                nuts: 'Westminster',
-                pfa: 'Metropolitan Police',
-                codes: {
-                    admin_district: 'E09000033',
-                    admin_county: 'E99999999',
-                    admin_ward: 'E05013806',
-                    parish: 'E43000236',
-                    parliamentary_constituency: 'E14000639',
-                    ccg: 'E38000256',
-                    ccg_id: 'W2U3Z',
-                    ced: 'E99999999',
-                    nuts: 'TLI32',
-                    lsoa: 'E01004736',
-                    msoa: 'E02000977',
-                    lau2: 'E09000033',
-                    pfa: 'E23000001'
-                }
-            }
+    result: {
+        postcode: 'SW1A 2AA',
+        quality: 1,
+        eastings: 530047,
+        northings: 179951,
+        country: 'England',
+        nhs_ha: 'London',
+        longitude: -0.127695,
+        latitude: 51.50354,
+        european_electoral_region: 'London',
+        primary_care_trust: 'Westminster',
+        region: 'London',
+        lsoa: 'Westminster 018C',
+        msoa: 'Westminster 018',
+        incode: '2AA',
+        outcode: 'SW1A',
+        parliamentary_constituency: 'Cities of London and Westminster',
+        admin_district: 'Westminster',
+        parish: 'Westminster, unparished area',
+        admin_county: null,
+        date_of_introduction: '198001',
+        admin_ward: 'St. James\'s',
+        ced: null,
+        ccg: 'NHS North West London',
+        nuts: 'Westminster',
+        pfa: 'Metropolitan Police',
+        codes: {
+            admin_district: 'E09000033',
+            admin_county: 'E99999999',
+            admin_ward: 'E05013806',
+            parish: 'E43000236',
+            parliamentary_constituency: 'E14000639',
+            ccg: 'E38000256',
+            ccg_id: 'W2U3Z',
+            ced: 'E99999999',
+            nuts: 'TLI32',
+            lsoa: 'E01004736',
+            msoa: 'E02000977',
+            lau2: 'E09000033',
+            pfa: 'E23000001'
         }
-    },
-    ok: true,
-    redirected: false,
-    headers: {} as Headers,
-    statusText: '',
-    type: 'default',
-    body: null,
-    url: '',
-    clone: () => {
-        return {} as Response
-    },
-    bodyUsed: false,
-    arrayBuffer: async () => {
-        return {} as ArrayBuffer
-    },
-    blob: async () => {
-        return {} as Blob
-    },
-    formData: async () => {
-        return {} as FormData
-    },
-    text: async () => {
-        return ''
+    }
+}
+
+export const TEST_POSTCODE_API_FETCH_RESPONSE = {
+    ...TEST_200_FETCH_RESPONSE,
+    json: async () => {
+        return TEST_POSTCODE_API_RESPONSE
     }
 }
 
 export const TEST_INVALID_POSTCODE = 'ZZ42BF'
-export const TEST_INVALID_POSTCODE_API_RESPONSE: Response = {
-    status: 200,
+export const TEST_INVALID_POSTCODE_API_RESPONSE = {
+    status: 404,
+    error: 'Postcode not found'
+}
+export const TEST_INVALID_POSTCODE_API_FETCH_RESPONSE = {
+    ...TEST_200_FETCH_RESPONSE,
     json: async () => {
-        return {
-            status: 404,
-            error: 'Postcode not found'
-        }
-    },
-    ok: true,
-    redirected: false,
-    headers: {} as Headers,
-    statusText: '',
-    type: 'default',
-    body: null,
-    url: '',
-    clone: () => {
-        return {} as Response
-    },
-    bodyUsed: false,
-    arrayBuffer: async () => {
-        return {} as ArrayBuffer
-    },
-    blob: async () => {
-        return {} as Blob
-    },
-    formData: async () => {
-        return {} as FormData
-    },
-    text: async () => {
-        return ''
+        return TEST_INVALID_POSTCODE_API_RESPONSE
+    }
+}
+
+export const TEST_POLICE_API_RESPONSE_DATA = [{}]
+
+export const TEST_POLICE_API_FETCH_RESPONSE = {
+    ...TEST_200_FETCH_RESPONSE,
+    json: async () => {
+        return [...TEST_POLICE_API_RESPONSE_DATA]
     }
 }
--- src/server/tests/utils/getCoords.test.ts
+++ src/server/tests/utils/getCoords.test.ts
@@ -2,8 +2,8 @@ import { when } from 'jest-when'
 import { getCoords } from '../../utils/getCoords'
 import {
     TEST_INVALID_POSTCODE,
-    TEST_INVALID_POSTCODE_API_RESPONSE,
-    TEST_POSTCODE_API_RESPONSE,
+    TEST_INVALID_POSTCODE_API_FETCH_RESPONSE,
+    TEST_POSTCODE_API_FETCH_RESPONSE,
     TEST_VALID_POSTCODE,
     TEST_VALID_POSTCODE_LONG_LAT
 } from '../testConstants'
@@ -13,10 +13,13 @@ const fetchSpy = jest.spyOn(global, 'fetch')
 describe('Testing the get coordinates function', () => {
     it('fetches the longitude and latitude for a given postcode and returns this in an array', async () => {
         when(fetchSpy)
-            .calledWith(`api.postcodes.io/postcodes/${TEST_VALID_POSTCODE}`, {
-                method: 'GET'
-            })
-            .mockResolvedValue(TEST_POSTCODE_API_RESPONSE)
+            .calledWith(
+                `https://api.postcodes.io/postcodes/${TEST_VALID_POSTCODE}`,
+                {
+                    method: 'GET'
+                }
+            )
+            .mockResolvedValue(TEST_POSTCODE_API_FETCH_RESPONSE)
         const response = await getCoords(TEST_VALID_POSTCODE)
         expect(response).toEqual(TEST_VALID_POSTCODE_LONG_LAT)
     })
@@ -26,7 +29,7 @@ describe('Testing the get coordinates function', () => {
             .calledWith(`api.postcodes.io/postcodes/${TEST_INVALID_POSTCODE}`, {
                 method: 'GET'
             })
-            .mockResolvedValue(TEST_INVALID_POSTCODE_API_RESPONSE)
+            .mockResolvedValue(TEST_INVALID_POSTCODE_API_FETCH_RESPONSE)
         const response = await getCoords(TEST_INVALID_POSTCODE)
         expect(response).toEqual('Invalid postcode, please try again')
     })
