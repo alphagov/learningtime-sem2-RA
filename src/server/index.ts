fa7f60cdc418c65d224b5ca70fc22eec867847e3
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-08-17 19:55:51 +0100

    added environment build

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,24 +13,44 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
-app.get('/', (_request: Request, response: Response) => {
-    response.json({ hello: 'world' })
-})
+const environment = process.env.NODE_ENV
 
-app.post('/api/postcode', async (request: Request, response: Response) => {
-    const { postcode, month } = request.body
-    const parsedPostcode: string = postcode.replace(' ', '').toUpperCase()
+if (environment === 'dev') {
+    app.post('/api/postcode', async (request: Request, response: Response) => {
+        const { postcode, month } = request.body
+        const parsedPostcode: string = postcode.replace(' ', '').toUpperCase()
 
-    // Process the request and prepare the response data
-    const coordsResponse = await getCoords(parsedPostcode)
-    if (Array.isArray(coordsResponse)) {
-        const policeAPIData = await getPoliceAPIData(coordsResponse, month)
-        response.send({ data: policeAPIData, coords: coordsResponse })
-    } else {
-        response.json({ data: coordsResponse })
-    }
-    // Send the response data back to the client
-})
+        // Process the request and prepare the response data
+        const coordsResponse = await getCoords(parsedPostcode)
+        if (Array.isArray(coordsResponse)) {
+            const policeAPIData = await getPoliceAPIData(coordsResponse, month)
+            response.send({ data: policeAPIData, coords: coordsResponse })
+        } else {
+            response.json({ data: coordsResponse })
+        }
+        // Send the response data back to the client
+    })
+} else if (environment === 'build') {
+    app.use('/assets', express.static(path.join(__dirname, '../client/assets')))
+    app.get('/', (_req, res) => {
+        res.sendFile(path.join(__dirname, '../client/index.html'))
+    })
+
+    app.post('/api/postcode', async (request: Request, response: Response) => {
+        const { postcode, month } = request.body
+        const parsedPostcode: string = postcode.replace(' ', '').toUpperCase()
+
+        // Process the request and prepare the response data
+        const coordsResponse = await getCoords(parsedPostcode)
+        if (Array.isArray(coordsResponse)) {
+            const policeAPIData = await getPoliceAPIData(coordsResponse, month)
+            response.send({ data: policeAPIData, coords: coordsResponse })
+        } else {
+            response.json({ data: coordsResponse })
+        }
+        // Send the response data back to the client
+    })
+}
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
